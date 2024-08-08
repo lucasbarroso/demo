c6b5cd091ce7dbc3aab4a15670a0fab2b8e22aa6
lucasbarroso <lucas.barrooso@gmail.com>
2024-08-08 14:16:36 -0300

    feat: adding get and setters in ProductModel entity

--- src/main/java/com/example/demo/models/ProductModel.java
+++ src/main/java/com/example/demo/models/ProductModel.java
@@ -16,9 +16,35 @@ public class ProductModel implements Serializable { //interface de marcação:ha
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)  //AUTO= autoincrement
+    @GeneratedValue(strategy= GenerationType.AUTO)  //AUTO= autoincrement
     private UUID idProduct;
     private String name;
     private String description;
     private BigDecimal value;
+    
+    
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public BigDecimal getValue() {
+        return value;
+    }
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
